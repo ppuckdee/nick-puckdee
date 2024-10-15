21a0a1a262afd1b5c09cf7dec78ec836e067cf5d
Patcharapol Puckdee <132693105+ppuckdee@users.noreply.github.com>
2024-10-15 15:54:02 -0700

    font change, added colors to button, night/day toggling feature

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,10 +1,21 @@
 import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/hero-img.png";
-import themeIcon from "../../assets/sun.svg";
-import linkedinIcon from "../../assets/linkedin-light.svg";
-import githubIcon from "../../assets/github-light.svg";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import CV from "../../assets/puckdee_cv.pdf";
+import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === "light" ? sun : moon;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+
   return (
     <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
@@ -17,6 +28,7 @@ function Hero() {
           className={styles.colorMode}
           src={themeIcon}
           alt="Color mode icon"
+          onClick={toggleTheme}
         />
       </div>
       <div className={styles.info}>
@@ -26,14 +38,23 @@ function Hero() {
         </h1>
         <h2>UX/UI Frontend Developer</h2>
         <span>
-          <a href="https://www.linkedin.com/in/nicholas-puckdee-068b692a2" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/nicholas-puckdee-068b692a2"
+            target="_blank"
+          >
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
           <a href="https://github.com/ppuckdee" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
         </span>
-        
+        <p>
+          With a passion for developing modern React web apps and UX/UI Design
+          for commercial businesses.
+        </p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
       </div>
     </section>
   );
