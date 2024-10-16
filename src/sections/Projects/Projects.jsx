70871829038cc5e930577b3ff7e052bc7c0d69ca
Patcharapol Puckdee <132693105+ppuckdee@users.noreply.github.com>
2024-10-16 12:00:16 -0700

    finished projects section, added skills section (dark/light mode)... now working on contact form section

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,16 +1,30 @@
 import styles from "./ProjectsStyles.module.css";
-import harmony from "../../assets/harmony.png"
+import harmony from "../../assets/harmony.png";
+import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <a href="https://www.canva.com/design/DAGHEdKpBZg/xYmwsiJcK4ZvwhLLryp3mw/edit?utm_content=DAGHEdKpBZg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target="_blank">
-        <img className="hover" src={harmony} alt="Harmony Logo" />
-        <h3>Harmony</h3>
-        <p>Work-life balance app design</p>
-        </a>
+        <ProjectCard
+          src={harmony}
+          link="https://www.canva.com/design/DAGHEdKpBZg/xYmwsiJcK4ZvwhLLryp3mw/edit?utm_content=DAGHEdKpBZg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+          h3="Harmony"
+          p="Work-life balance app design"
+        />
+        <ProjectCard
+          src={harmony}
+          link="https://www.canva.com/design/DAGHEdKpBZg/xYmwsiJcK4ZvwhLLryp3mw/edit?utm_content=DAGHEdKpBZg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+          h3="Harmony"
+          p="Work-life balance app design"
+        />
+        <ProjectCard
+          src={harmony}
+          link="https://www.canva.com/design/DAGHEdKpBZg/xYmwsiJcK4ZvwhLLryp3mw/edit?utm_content=DAGHEdKpBZg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+          h3="Harmony"
+          p="Work-life balance app design"
+        />
       </div>
     </section>
   );
