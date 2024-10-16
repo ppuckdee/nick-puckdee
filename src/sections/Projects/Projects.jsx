90be1b9044881f46d934e0569d54758a88cd4384
Patcharapol Puckdee <132693105+ppuckdee@users.noreply.github.com>
2024-10-16 10:59:32 -0700

    changed harmony app icon, added description to app, styled in css, fixed positioning and alignment of text, created hover animation (icon enlarges when hovered over)

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -6,8 +6,10 @@ function Projects() {
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <a href="https://www.canva.com/design/DAGHEdKpBZg/xYmwsiJcK4ZvwhLLryp3mw/edit?utm_content=DAGHEdKpBZg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">
+        <a href="https://www.canva.com/design/DAGHEdKpBZg/xYmwsiJcK4ZvwhLLryp3mw/edit?utm_content=DAGHEdKpBZg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target="_blank">
         <img className="hover" src={harmony} alt="Harmony Logo" />
+        <h3>Harmony</h3>
+        <p>Work-life balance app design</p>
         </a>
       </div>
     </section>
