5a603d54caa8ffd58b0b98d2fec16a8cd43699de
Patcharapol Puckdee <132693105+ppuckdee@users.noreply.github.com>
2024-10-15 17:04:59 -0700

    updated project section (harmony ux design project), added new logo, changed name of file in assets folder.

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,13 +1,13 @@
 import styles from "./ProjectsStyles.module.css";
-import darkthrone from "../../assets/darkthrone.png"
+import harmony from "../../assets/harmony.png"
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <a href="https://github.com/esaltzherr/Dark-Throne">
-        <img className="hover" src={darkthrone} alt="Dark Throne Logo" />
+        <a href="https://www.canva.com/design/DAGHEdKpBZg/xYmwsiJcK4ZvwhLLryp3mw/edit?utm_content=DAGHEdKpBZg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">
+        <img className="hover" src={harmony} alt="Harmony Logo" />
         </a>
       </div>
     </section>
